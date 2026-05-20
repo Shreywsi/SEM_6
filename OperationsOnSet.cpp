07b617b9dee151207a9b457bbaf076cad42b666c
Shreywsi <b230029@nitsikkim.ac.in>
2026-05-20 22:50:23 +0530

    Convert C++ set operations to Python syntax
    Refactored OperationsOnSet.cpp to use Python-like syntax for set operations and removed C++ specific input/output.

--- OperationsOnSet.cpp
+++ OperationsOnSet.cpp
@@ -1,58 +1,16 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <iterator>
-using namespace std;
+a = input("Enter elements of Set A (space separated): ")
+A = set(map(int, a.split()))
+b = input("Enter elements of Set B (space separated): ")
+B = set(map(int, b.split()))
 
-int main()
-{
-    int s1, s2;
-    cout << "enter the length of set 1: ";
-    cin >> s1;
-    cout << "enter the length of set 2: ";
-    cin >> s2;
+print("\nSet A:", A)
+print("Set B:", B)
 
-    vector<int> sn1(s1), sn2(s2);
+# Union
+print("\nUnion:", A | B)
 
-    for (int i = 0; i < s1; i++)
-    {
-        cout << "enter the no. for set 1: ";
-        cin >> sn1[i];
-    }
+# Intersection
+print("Intersection:", A & B)
 
-    for (int i = 0; i < s2; i++)
-    {
-        cout << "enter the no. for set 2: ";
-        cin >> sn2[i];
-    }
-
-    sort(sn1.begin(), sn1.end());
-    sort(sn2.begin(), sn2.end());
-
-    vector<int> uni, intersection, difference;
-
-    set_union(sn1.begin(), sn1.end(),
-              sn2.begin(), sn2.end(),
-              back_inserter(uni));
-
-    set_intersection(sn1.begin(), sn1.end(),
-                     sn2.begin(), sn2.end(),
-                     back_inserter(intersection));
-
-    set_difference(sn1.begin(), sn1.end(),
-                   sn2.begin(), sn2.end(),
-                   back_inserter(difference));
-    cout <<"Union is: ";
-    for (int x : uni)
-        cout << x << " ";
-    cout << endl;
-    cout<<"intersection is: ";
-    for (int x : intersection)
-        cout<<x << " ";
-    cout << endl;
-    cout << "difference is: ";
-    for (int x : difference)
-        cout << x << " ";
-
-    return 0;
-}
+# Subtraction
+print("Subtraction:", A - B)
